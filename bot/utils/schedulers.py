f2b9e2dd0b2a6e813560413ca0c875c9a59e4d97
YaroslavPanasiuk <yaroslav.panasiuk@lnu.edu.ua>
2025-10-24 20:19:18 +0300

    fix

--- bot/utils/schedulers.py
+++ bot/utils/schedulers.py
@@ -6,7 +6,7 @@ from pytz import timezone
 scheduler = AsyncIOScheduler(timezone=timezone("Europe/Kyiv"))
 
 def init_jar_refresh_tasks(bot):
-    scheduler.add_job(refresh_jars_silent, "cron", hour="0,7,9,11,13,15,17,19,20,21,22,23", args=[bot])
+    scheduler.add_job(refresh_jars_silent, "cron", hour="0,7,9,11,13,15,17,19,20,21,22,23")
     scheduler.start()
 
 def list_jobs():
